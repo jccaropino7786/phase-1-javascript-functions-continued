1d3f4a40eeb835b9c3adf3ba653227912108bec1
john Caropino <jccaropino7786@live.com>
2022-11-27 14:44:18 -0800

    commit tree 6a652ec7f49f18b4140848444feed41ded657e84

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+function saturdayFun(argument="roller-skate"){
+    return (`This Saturday, I want to ${argument}!`)
+  
+}
+
+saturdayFun("bathe my dog")
+
+function mondayWork(x="go to the office"){
+    return (`This Monday, I will ${x}.`)
+}
+mondayWork('work from home')
+
+function wrapObjective(para='*'){
+    return (`You are ${para}!`)
+}
